575b1b0d3bd3222c33d020e6b7d58f787e74ffd1
Vladimir Artyushin <artyushin@inbox.ru>
2020-08-02 13:25:25 +0300

    endActivity

---
+++ app/src/main/java/com/artyushin/hw30/VitalSigns.java
@@ -0,0 +1,14 @@
+package com.artyushin.hw30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VitalSigns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vital_signs);
+    }
+}
---
+++ app/src/main/java/com/artyushin/hw30/xResume.java
@@ -0,0 +1,14 @@
+package com.artyushin.hw30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class xResume extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_x_resume);
+    }
+}
